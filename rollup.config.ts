5e208ac5aa4ea9b31a3327beb736da6e17a8e661
shanejixx <shanejix@vip.163.com>
2020-08-18 20:56:21 +0800

    [build] 0.0.1

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'over-axios'
+const libraryName = 'over-ts-axios'
 
 export default {
   input: `src/${libraryName}.ts`,
